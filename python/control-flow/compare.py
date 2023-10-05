52e25b4b9cc345a2f235972e3ea9ddd3aa19a0ec
mcelle888 <14894@coderacademy.edu.au>
2023-10-05 15:01:16 +1100

    control-flow

---
+++ python/control-flow/compare.py
@@ -0,0 +1,38 @@
+# x = int(input('What is X?'))
+# y = int(input('What is Y?'))
+
+# if x < y:
+#     print('x is less than y')
+# elif x > y:
+#     print('x is greater than y')
+# else: 
+#     print ('x is equal to y')
+
+ 
+# score = int(input('Score '))
+
+# if score >= 90:
+#     print('HD')
+# elif score >=80:
+#     print('D')
+# elif score >=70:
+#     print ('CR')
+# elif score >=50:
+#     print ('P')
+# else:
+#     print ('F')
+
+# name = input('What is your name?')
+
+
+# match name: 
+#     case 'Harry' | 'Ron' | 'Hermione':
+#         print ('Gryffindore')
+#     case 'Ron':
+#         print('Gryffindore')
+#     case 'Draco':
+#         print ('Slytherin')
+#     case _:
+#         print ('Mudblood!')
+
+
