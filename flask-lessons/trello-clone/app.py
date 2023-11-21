4baa7d30b14a6433fa13179de86ffc0f9243394b
mcelle888 <14894@coderacademy.edu.au>
2023-11-21 14:17:11 +1100

    authentication

--- flask-lessons/trello-clone/app.py
+++ flask-lessons/trello-clone/app.py
@@ -1,10 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
-
-import json
 from flask_marshmallow import Marshmallow
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 # CREATE configurations (set up database uri in the config)
@@ -20,7 +18,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
-
+bcrypt = Bcrypt(app)
 
 
 # now we define models (we define and declare our columns in python as classes and the orm, when we call method eg get all the users, the orm takes care of sql and brings data to us to use in python)
@@ -47,6 +45,23 @@ class CardSchema(ma.Schema):
     class Meta:
         fields = ('id', 'title', 'description', 'status', 'date_created')
 
+# AUTHENTICATION
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, nullable = False, unique = True)
+    password = db.Column(db.String, nullable = False)
+    is_admin = db.Column(db.Boolean, default = False)
+
+# USER SCHEMA
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'email', 'password', 'is_admin')
+
 
 # we've declared the class but now we need to define all the models we need. Once these declarations are done, to create the tables in the physical database, we need to call the create all method. What we're declaring here is a custom terminal command
 
@@ -60,6 +75,22 @@ def db_create():
 # Seeding a database: inserting some test data for development purposes into our database (So we have data to work with)
 @app.cli.command('db_seed')
 def db_seed():
+
+
+    users = [
+        User(
+            email = 'admin@spam.com',
+            password = bcrypt.generate_password_hash ('spinynorman').decode('utf8'),
+            is_admin = True
+        ),
+        User(
+            name = 'John Cleese',
+            email = 'cheese@spam.com',
+            password = bcrypt.generate_password_hash ('tisbutascratch').decode('utf8'),
+        )
+    ]
+
+
     cards = [
     Card(
         title = 'Start of project',
@@ -85,6 +116,7 @@ def db_seed():
 
 
 # 'session' creates a database transaction: 
+    db.session.add_all(users)
     db.session.add_all(cards)
     # now we must commit after writing all the edits
     db.session.commit()
@@ -93,6 +125,22 @@ def db_seed():
 # We've created an instance in memory (RAM) but we havent put it into the database yet (wont do it automatically because in some instances, we may want to do other things before commiting to database) So how do we commit to the database? We add in line 48. 
 
 
+@app.route('/users/register', methods = ['POST'])
+def register():
+    # parse incoming POST body through the schema
+    user_info = UserSchema(exclude=['id']).load(request.json)
+    # create new user with parsed data
+    user = User(
+        email = user_info['email'], 
+        password = bcrypt.generate_password_hash(user_info['password']).decode('utf8'),
+        name = user_info.get('name', '')
+    )
+    print(user)
+    # add and commit new user to the database
+    db.session.add(user)
+    db.session.commit()
+    # return the new user
+    return UserSchema(exclude= ['password']).dump(user), 201
 
 # QUERY
 @app.route('/cards')
@@ -104,7 +152,7 @@ def all_cards():
     # stmt = db.select(Card).limit(1)
 
     # select more specific like the where statement in sql
-    stmt = db.select(Card).where(Card.id < 3)
+    # stmt = db.select(Card).where(Card.id < 3)
 
     # select cards that are not done AND card id is greater than 2
     # stmt = db.select(Card).where(Card.status != 'Done', cards.id > 2)
@@ -116,7 +164,7 @@ def all_cards():
     # Sorted ascending by card title by adding the .order_by
     # stmt = db.select(Card).where(Card.id < 3).order_by(Card.title)
     # # descending order
-    # stmt = db.select(Card).where(Card.id < 3).order_by(Card.title.desc())
+    stmt = db.select(Card).where(db.or_(Card.status != 'Done', Card.id > 2)).order_by(Card.title.desc())
 
     # scalars method telle alchemy to execute the statement and creates instances of the class for us
     cards = db.session.scalars(stmt).all()
@@ -131,11 +179,7 @@ def all_cards():
     #     # more specific data:
     #     print(card.title)
     return CardSchema(many = True).dump(cards)
-
-    
-
-
-
+# this converts it to a list of dictionaries (to primative python data types which are easily serialized to json which is done by flask)
 
 
 @app.route('/')
