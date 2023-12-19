da171c1f8099a8b0bf2c148d0b2ca5f9767479d0
mcelle888 <14894@coderacademy.edu.au>
2023-12-19 14:46:11 +1100

    intro to js

---
+++ javascript/intro/script.js
@@ -0,0 +1,59 @@
+let str = 'Hello world!'
+
+console.log(str.replaceAll('o', '---'))
+ 
+
+// Python: print(f'Hello, {name}!')
+
+// console.log(`Hello, ${str}!`)
+// console.log(`Answer: ${5*10}`)
+
+
+// console.log(Math.floor(3 / 2))
+
+// let x = 5
+// console.log(x)
+
+//Increment X (Python: x += 1)
+// x += 1
+// x++
+// x--
+
+// console.log(x++)
+// console.log(x)
+
+// console.log(123 === "123")
+
+
+// let x = 5
+// console.log(typeof x)
+
+
+// Python: person = {"name": "Michelle"}
+
+// let person = {
+//     "name": "Michelle",
+//     "age": 27
+//     }
+
+// let key = 'name'
+
+// console.log(person[key])
+
+
+// console.log(person.age)
+
+
+// x = [1, 2, 3, 3.14159, true, 'hello']
+
+// console.log(x)
+
+// console.log(x[x.length-1])
+
+
+// python:
+// def add(x,y):
+//  return x + y 
+
+function add(x,y) {return x + y}
+console.log(add(10, 10))
